aefd326e1bc0bb6044c9cd7dedcb55946a0764e7
mayestroga <mayestroga3@gmail.com>
2026-01-23 16:30:50 +0800

    feat: added components

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,9 @@ import LoginPage from '@/Authentication/LoginPage.vue'
 import Dashboard from '@/Pages/DashBoard.vue'
 import CommunityView from '@/Pages/CommunityView.vue'
 import LandingPage from '@/Pages/LandingPage.vue'
+import Schools from '@/Components/Schools.vue'
+import Churches from '@/Components/Churches.vue'
+import Establishments from '@/Components/Establishments.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -10,6 +13,13 @@ const router = createRouter({
     { name: 'login', path: '/login', component: LoginPage },
     { name: 'dashboard', path: '/dashboard', component: Dashboard },
     { name: 'CommunityView', path: '/community/:barangayId', component: CommunityView },
+    { name: 'schools', path: '/community/:barangayId/schools', component: Schools },
+    { name: 'churches', path: '/community/:barangayId/churches', component: Churches },
+    {
+      name: 'establishments',
+      path: '/community/:barangayId/establishments',
+      component: Establishments,
+    },
     { name: 'landing', path: '/', component: LandingPage}
   ],
 })
